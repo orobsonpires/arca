2e23777cae88b07b2ae37cc331f8aef6d2119c7b
Rob <orobsonpires@gmail.com>
2017-04-17 22:28:05 +0200

    update categoty to include category type

---
+++ models/categoryType.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CategoryTypeSchema = new Schema({
+	id : {
+		type : String,
+		required : true,
+		unique : true
+	},
+	name : {
+		type : String,
+		required : true,
+		unique : true
+	},
+	description : {
+		type : String
+	}
+});
+
+module.exports = mongoose.model('CategoryType', CategoryTypeSchema);
